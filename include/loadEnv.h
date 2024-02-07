706cd17ca6968191dea3f50bae8beef946769745
Pawel Sawicki <public@psawicki.de>
2024-02-07 19:10:01 +0100

    Add support for loading environment variables from multiple files

--- include/loadEnv.h
+++ include/loadEnv.h
@@ -1,9 +1,16 @@
 #ifndef LOADENV_H
 #define LOADENV_H
 
+#include <concepts>
+#include <array>
 #include <unordered_map>
 #include <string>
+#include <vector>
 
 std::unordered_map<std::string, std::string> loadEnv(const std::string& filename);
+
+template<std::ranges::range Range>
+    requires std::same_as<std::ranges::range_value_t<Range>, std::string>
+std::unordered_map<std::string, std::string> loadEnv(const Range& filenames);
 
 #endif // LOADENV_H
--- src/loadEnv.cpp
+++ src/loadEnv.cpp
@@ -2,11 +2,19 @@
 #include <fstream>
 #include <sstream>
 
-// Function to load environment variables from .env file
-std::unordered_map<std::string, std::string> loadEnv(const std::string& filename) {
-    
+extern char** environ;
 
-    std::unordered_map<std::string, std::string> env;
+void load_from_sys_env(std::unordered_map<std::string, std::string>& env) {
+    for (char** envp = environ; *envp != nullptr; ++envp) {
+        std::string env_str(*envp);
+        const auto delimiter_position = env_str.find('=');
+        std::string key = env_str.substr(0, delimiter_position);
+        std::string value = env_str.substr(delimiter_position + 1);
+        env[key] = value;
+    }
+}
+
+void load_from_file(const std::string& filename, std::unordered_map<std::string, std::string>& env) {
     std::ifstream file(filename);
     std::string line;
 
@@ -21,5 +29,25 @@ std::unordered_map<std::string, std::string> loadEnv(const std::string& filename
             }
         }
     }
+}
+
+// Function to load environment variables from .env file
+std::unordered_map<std::string, std::string> loadEnv(const std::string& filename) {
+
+    std::unordered_map<std::string, std::string> env;
+    load_from_file(filename, env);
+    load_from_sys_env(env);
+    
+    return env;
+}
+
+template<std::ranges::range Range>
+    requires std::same_as<std::ranges::range_value_t<Range>, std::string>
+std::unordered_map<std::string, std::string> loadEnv(const Range& filenames) {
+    std::unordered_map<std::string, std::string> env;
+    for (const auto& filename : filenames) {
+        load_from_file(filename, env);
+    }
+    load_from_sys_env(env);
     return env;
 }
